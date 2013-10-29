ca5fd4c5a84ab3c2f0d577d2091ec21ff310bba8
Cahlan Sharp <cahlan@gmail.com>
2013-10-29 13:59:42 -0600

    adds initial files

---
+++ FriendController.js
@@ -0,0 +1,23 @@
+app.controller('FriendController', function($scope){
+    
+    $scope.friends = [];
+    $scope.totalFriends = 0;
+    $scope.nameFilter = "";
+    $scope.locationFilter = "";
+    $scope.orderReverse = "false";
+    $scope.orderAttribute = 'name';
+    var handleApiResponse = function(response){
+            var friends = response.results;
+            angular.forEach(friends, function (friend) {
+                friend.friend_count = parseInt(friend.friend_count);
+            });
+            console.log(friends);
+            $scope.totalFriends = friends.length;
+            $scope.friends = friends;
+            $scope.$apply();
+    };
+    var url = 'https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json';
+
+    $.getJSON(url, handleApiResponse);
+
+});
